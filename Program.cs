12a38069444c47ce956e3853fe32caadc76646e5
Jon Connell <con18013@byui.edu>
2022-04-27 14:15:14 -0600

    display board, accept first input, update board

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initialize variables for gameplay
+            char current_turn = 'x';
+            bool game_over = false;
+
+            // Create and display starting game board
+            List<char> board = new List<char> {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            DisplayBoard(board);
+
+            // Loop through game until there is a winner or tie
+            while (game_over != true)
+            {
+                // Display whose turn it is
+                Console.Write($"{current_turn}'s turn to choose a square (1-9): ");
+
+                // Get input from user
+                string player_input = Console.ReadLine();
+                int num_input = Int32.Parse(player_input);
+
+                // Update board
+                board[num_input-1] = current_turn;
+                DisplayBoard(board);
+
+                // Check for winner (change turn)
+
+            }
+
+        }
+
+        static void DisplayBoard(List<char> board)
+        {
+            // Display game board using current values from 'board' list
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}\n-+-+-\n{board[3]}|{board[4]}|{board[5]}\n-+-+-\n{board[6]}|{board[7]}|{board[8]}");
         }
     }
 }
